73a714b5c5d2da8ea8b1fc0701493770cce862c2
anujsoni08 <anujsoni08@gmail.com>
2020-11-12 01:59:42 +0530

    added signin signup

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,20 +1,60 @@
-import React, { useState } from "react";
+import React from "react";
 
-import { sections } from "../../constants/directory.data";
 import MenuItem from "../menu-item/menu-item.component";
 
 import "./directory.styles.scss";
 
-const Directory = () => {
-  const [sectionList, setSectionList] = useState(sections);
+class Directory extends React.Component {
+  constructor() {
+    super();
 
-  return (
-    <div className="directory-menu">
-      {sectionList.map(({ title, imageUrl, id, size }) => (
-        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-      ))}
-    </div>
-  );
-};
+    this.state = {
+      sections: [
+        {
+          title: "hats",
+          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          id: 1,
+          linkUrl: "hats",
+        },
+        {
+          title: "jackets",
+          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          id: 2,
+          linkUrl: "",
+        },
+        {
+          title: "sneakers",
+          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          id: 3,
+          linkUrl: "",
+        },
+        {
+          title: "womens",
+          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          size: "large",
+          id: 4,
+          linkUrl: "",
+        },
+        {
+          title: "mens",
+          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          size: "large",
+          id: 5,
+          linkUrl: "",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Directory;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,18 +1,28 @@
 import React from "react";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
-import { shopData } from "../../constants/directory.data";
+import SHOP_DATA from "./shop.data.js";
 
-const ShopPage = () => {
-  const collections = shopData;
+import CollectionPreview from "../../components/collection-preview/collection-preview";
 
-  return (
-    <div className="shop-page">
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
-    </div>
-  );
-};
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
 
 export default ShopPage;
